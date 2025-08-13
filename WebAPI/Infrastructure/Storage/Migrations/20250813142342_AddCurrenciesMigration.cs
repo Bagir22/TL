9b7d4a06bd537dc7bc74e49cd9da0705c203a4fb
Bagir22 <bagir2202@gmail.com>
2025-08-13 17:26:51 +0300

    Добавил валюты в Currency таблицу

---
+++ WebAPI/Infrastructure/Storage/Migrations/20250813142342_AddCurrenciesMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrenciesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currency",
+                columns: new[] { "Type"},
+                values: new object[,]
+                {
+                    { "RUB" },
+                    { "USD" },
+                    { "EUR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currency",
+                keyColumn: "Type",
+                keyValues: new object[] { "RUB" });
+
+            migrationBuilder.DeleteData(
+                table: "Currency",
+                keyColumn: "Type",
+                keyValues: new object[] { "USD" });
+
+            migrationBuilder.DeleteData(
+                table: "Currency",
+                keyColumn: "Type",
+                keyValues: new object[] { "EUR" });
+        }
+    }
+}
